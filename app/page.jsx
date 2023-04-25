11fec225316f0e099aea8042677f16db4e161e59
2000rick <qipeng.liang.edu@gmail.com>
2023-04-25 16:20:14 -0700

    Hotfix Reuse Experience componenet, delete projects componenet

--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,13 @@ import AboutMe from './component/about-me'
 import { aboutMe, skills, professionalData, projectData } from "../data/page-data";
 import Experience from './component/experience';
 import Skills from './component/skills';
-import Projects from './component/projects';
 
 const page = () => {
   return (
     <div>
       <Skills data={skills} />
       <Experience data={professionalData} />
-      <Projects data={projectData} />
+      <Experience data={projectData} />
       <AboutMe data={aboutMe}/>
     </div>
   )
